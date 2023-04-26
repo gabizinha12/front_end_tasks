54c7a42bde9fa42e7321ba858ab7172ed5117731
Gabrielle Lima <gabrielle.clima23@gmail.com>
2023-04-25 22:29:02 -0300

    feat: Update front-end: GET Request working

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
-const tasksDiv = document.getElementById('tasksDiv');
+var tasksDiv = document.getElementById("tasksDiv");
 
-function fetchTasks() {
-    return fetch('http://localhost:8080/tasks')
-        .then(response => {
-                console.log(response); // Logs the response
-                return response.body;
-                }
-              );
-   }
-   console.log(fetchTasks()); // Returns Promise
+function listarTarefas() {
+  $.ajax({
+    url: "http://localhost:8080/tasks",
+    type: "get",
+    dataType: "json",
+    success: function (data) {
+      console.log(data);
+      var ul = document.createElement("ul");
+      for (var i in data) {
+        var li = document.createElement("li");
+        var pDescription = document.createElement("p");
+        var pDeadline = document.createElement("p");
+        li.innerHTML = JSON.stringify(data[i].title);
+        pDescription.innerHTML = JSON.stringify(data[i].description);
+        pDeadline.innerHTML = JSON.stringify(data[i].deadline);
+        ul.appendChild(li);
+        ul.appendChild(pDescription);
+        ul.appendChild(pDeadline);
+        tasksDiv.appendChild(ul);
+      }
+      console.log(data);
+    },
+  });
+}
+
+function criarTarefa() {
+  console.log("Criar tarefa");
+}
