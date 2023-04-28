69738dfa4aacf79ccefb00e033ee9889d1178e57
Gabrielle Lima <gabrielle.clima23@gmail.com>
2023-04-28 00:49:53 -0300

    feat: Update front-end: POST Request not-working

--- js/index.js
+++ js/index.js
@@ -26,8 +26,7 @@ function listarTarefas() {
 }
 
 function criarTarefa() {
-  console.log("Criar tarefa");
-  var form = $(".mainForm").serializeArray();
+  var form = $("form").serializeArray();
   var obj = {"title": form[0].value, "description": form[1].value, "deadline": form[2].value}
   var string = JSON.stringify(obj);
   $.ajax({
@@ -36,7 +35,16 @@ function criarTarefa() {
     url: "http://localhost:8080/tasks" + "/task/create",
     contentType: "application/json",
     data: string,
-    success: function(data) {
+    success: function(data, event) {
+      event.preventDefault();
+      const newTasks = document.getElementById("newTasks");
+      var ul = document.createElement("ul");
+      for(var i in data) {
+        var li = document.createElement("li");
+        li.innerHTML = JSON.stringify(data[i].deadline);
+        ul.appendChild(li);
+        newTasks.appendChild(ul);
+      }
     console.log(data);
     }
   });
